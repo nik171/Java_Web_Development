04cb40ae084fcb8b5afe5615948136f2d2b928e3
nik171 <filatovni@mail.ru>
2018-07-06 21:52:30 +0300

    Add files via upload

---
+++ First_Task_ChapterA/Point3D.java
@@ -0,0 +1,79 @@
+package by.epam.task1chpta.entities;
+
+public class Point3D {
+    private double x;
+    private double y;
+    private double z;
+
+    public Point3D(){}
+
+    public Point3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Point3D(Point3D copy) {
+        this.x = copy.getX();
+        this.y = copy.getY();
+        this.z = copy.getZ();
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point3D point3D = (Point3D) o;
+
+        if (Double.compare(point3D.x, x) != 0) return false;
+        if (Double.compare(point3D.y, y) != 0) return false;
+        return Double.compare(point3D.z, z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(z);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+}
